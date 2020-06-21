f11daa492cf0ad052f68d6e76cfef327c6e3a858
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-06-21 09:57:47 -0700

    create timer()

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,30 @@
     //use getElementById() and querySelector()
 
 //write functions
+function myTimer() {
+    //set variables
+    var startTime = 5;
+    var myInterval = 3000;
+    var timer = document.getElementById("timer");
+
+    //The set interval takes two arguments,  the function and an interval
+    var mySet = setInterval( createTimer, myInterval);
+
+    function createTimer() {
+        //Keep the interval going IF your startTime is above zero
+        if (startTime > 0) {
+            timer.textContent = startTime;
+    
+        //Subtracts 1 from the start time
+        startTime = startTime - 1
+    
+        } else {
+        timer.textContent = "FATALITY"
+
+        clearInterval(mySet)
+        }
+    }
+}
     //startGame()
     //setQuestion()
 
