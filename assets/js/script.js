86078e146efab90a2f4f1ff34bf13073c2a62b91
McClellan-Micah <mcclellan.micah.6@gmail.com>
2020-06-20 17:58:40 -0700

    pseudocode javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,13 @@
+//timer function should wrap everything
+
 //declare variables
+    //array containing questions and answers
+    //choices 1,2,3,4
+    //use getElementById() and querySelector()
 
 //write functions
+    //startGame()
+    //setQuestion()
 
 //addEventListeners
+    //at least one for the button for startGame when "click"
